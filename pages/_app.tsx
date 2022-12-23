85bbdd0c29bf7f530698e9a80c3704bf67c2385b
shaiq-dev <shaiqkar@gmail.com>
2022-12-24 03:53:29 +0530

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,17 @@
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 import GlobalStyle from 'styles/global'
-import GlobalErrorStyle from 'styles/global.errors.styled'
 import HomeLayout from 'layout/index'
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log(pageProps)
-
-  // Error page is not wrapped with layout
-  if (pageProps.__isLayoutDisabled) {
-    return (
-      <>
-        <GlobalErrorStyle />
-        <Component {...pageProps} />
-      </>
-    )
-  }
-
   return (
     <>
       <GlobalStyle />
       <HomeLayout>
         <Component {...pageProps} />
       </HomeLayout>
+      <Analytics />
     </>
   )
 }
