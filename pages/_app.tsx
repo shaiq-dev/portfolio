d1d9f7c12bc75869fe1240adf844046fbd9a8bed
shaiq-dev <shaiqkar@gmail.com>
2024-01-27 22:58:43 +0530

    Easter egg 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,12 @@ import { Analytics } from '@vercel/analytics/react'
 import GlobalStyle, { GlobalErrorStyle } from 'styles/global'
 import MainLayout from 'layouts/index'
 import { AppStrings } from 'constants/index'
+import { doodleInformation } from 'utils/console'
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Console easter eggs
+  doodleInformation()
+
   // Render error pages without any layout
   if (Component.displayName === AppStrings.ERROR_PAGE_DISPLAY_NAME) {
     return (
